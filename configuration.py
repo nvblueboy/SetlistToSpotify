f153289cff0fee1761565ba179d880cd79a63e83
nvblueboy <dbowman06@gmail.com>
2016-08-14 21:19:49 -0700

    Added to config parser.

--- configuration.py
+++ configuration.py
@@ -1,4 +1,14 @@
+import configparser, os
+
+
 class Configuration():
-    def __init__(self, default = "./config.ini"):
-        
+    def __init__(self, filename = "./config.ini"):
+        #Check if the file exists.
+        if os.path.isfile(filename):
+            #If so, parse it.
+            config = configparser.ConfigParser()
+            config.read(filename)
+            self.setlistKey = config["setlist.fm"]["key"]
+            
+            
         
